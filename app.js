5a1f3276c166fefce640d8ab6e7f0ffcfd244d09
Janos Kocs <janos.kocs@outlook.com>
2022-09-18 12:31:14 +0100

    DB connect and schema

--- app.js
+++ app.js
@@ -4,6 +4,34 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 require('dotenv').config()
 
+mongoose.connect('mongodb://127.0.0.1:27017/restaurantBooking')
+
+const bookingSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    seatCount: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        default Date.now
+    },
+    time: {
+        type: String,
+        required: true
+    },
+    reference: {
+        type: Number,
+        required: trusted,
+    },
+    email: {
+        type: String,
+        required: true
+    }
+})
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
